d05acdd743e872e7a65c20f628e37572e4ae773d
Celio Reyes <celioreyesrr@gmail.com>
2018-12-24 16:18:01 -0500

    Lesson 69: Getting Transforms in C++ - PositionReporter class now logs the Actor's position

--- BuildingEscape/Source/BuildingEscape/PositionReporter.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReporter.cpp
@@ -3,33 +3,29 @@
 #include "PositionReporter.h"
 #include "GameFramework/Actor.h"
 
+
 // Sets default values for this component's properties
-UPositionReporter::UPositionReporter()
-{
+UPositionReporter::UPositionReporter() {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
 }
 
 
 // Called when the game starts
-void UPositionReporter::BeginPlay()
-{
+void UPositionReporter::BeginPlay() {
 	Super::BeginPlay();
 
 	FString ObjectName = GetOwner()->GetName(); // Grab the name of the current GameObject
-	UE_LOG(LogTemp, Warning, TEXT("UPositionReporter Initing for %s!"), *ObjectName);
+	FString ObjectPos = GetOwner()->GetActorLocation().ToString(); // Gets the Vector location of current GameObject
+	UE_LOG(LogTemp, Warning, TEXT("%s at %s"), *ObjectName, *ObjectPos);
 
 }
 
 
 // Called every frame
-void UPositionReporter::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
-{
+void UPositionReporter::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
 }
-
