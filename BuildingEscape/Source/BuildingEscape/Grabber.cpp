02d8c53947249876d81400824e4ff137590a40ca
Celio Reyes <celioreyesrr@gmail.com>
2018-12-28 16:29:33 -0500

    Lesson 84: LineTraceSingleByObjectType() - Raycasting and filtering out object we don't want

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -21,25 +21,34 @@ void UGrabber::BeginPlay() {
 
 	// ...
 	UE_LOG(LogTemp, Warning, TEXT("Grabber | Running in BeginPlay()"));
+
+	
 }
 
 // Called every frame
 void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// OUT Params
+	/// OUT Params
 	FVector PLocation;
 	FRotator PRotation;
 
-	// Grab the player's viewpoint
+	/// Grab the player's viewpoint
 	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(OUT PLocation, OUT PRotation);
-	//UE_LOG(LogTemp, Warning, TEXT("Grabber | Player Location: %s | Player Viewpoint: %s"), *PLocation.ToString(), *PRotation.ToString());
 
 	FVector  LineTraceEnd = PLocation + PRotation.Vector() * Reach;
 	DrawDebugLine( GetWorld(), PLocation, LineTraceEnd, FColor(0, 255, 0), false, 0.f, 0, 10.f );
 	
-	// Ray-cast to reach distance
-
-	// Check what we hit
+	/// Set up Collision Query Params
+	FCollisionQueryParams TraceParams(FName(TEXT("")), false, GetOwner());
+	
+	/// Line trace (Ray-cast) to reach distance
+	FHitResult Hit;
+	GetWorld()->LineTraceSingleByObjectType(OUT Hit, PLocation, LineTraceEnd, FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody), TraceParams);
+	
+	/// Check what we hit
+	if (Hit.GetActor()) {
+		UE_LOG(LogTemp, Warning, TEXT("Ray Cast hitting: %s"), *Hit.GetActor()->GetName())
+	}
 }
 
